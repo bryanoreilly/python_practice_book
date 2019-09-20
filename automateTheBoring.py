0ba91bddaa3677a9a48213f4c2478a3960aadfaf
bco8upv <bryan_oreilly@homedepot.com>
2019-09-20 10:52:00 -0400

    Separating the boring from the practice files

---
+++ automateTheBoring.py
@@ -0,0 +1,4 @@
+#### AUTOMATE THE BORING STUFF #####
+#end changes the last character (in this case from newline to "")
+print("Hello", end="")
+print(" world")
--- bookExercises.py
+++ bookExercises.py
@@ -2,10 +2,22 @@
 #https://buildmedia.readthedocs.org/media/pdf/python-practice-book/latest/python-practice-book.pdf
 
 #Skipping ahead to test new concepts...
+##Lambdas
 def fxy(f, x, y):
     return f(x) + f(y)
 
 cube = lambda x: x ** 3
 
-print(fxy (cube, 2, 3))
-print(fxy(lambda x: x**4 , 2, 3))
+#print(fxy (cube, 2, 3))
+#print(fxy(lambda x: x**4 , 2, 3))
+
+##Lists
+x = [0, 1, [2]]
+x[2][0] = 3
+#print(x[2][0])
+x[2].append(4)
+#print(x)
+
+#for i in range(10):
+#    print (i, i*i, i*i*i)
+
