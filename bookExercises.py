ed97360976405a36183e88909219bd9848c9306d
bco8upv <bryan_oreilly@homedepot.com>
2019-09-23 19:18:33 -0400

    problem 36 and seashells

--- bookExercises.py
+++ bookExercises.py
@@ -12,12 +12,79 @@ cube = lambda x: x ** 3
 #print(fxy(lambda x: x**4 , 2, 3))
 
 ##Lists
-x = [0, 1, [2]]
-x[2][0] = 3
-#print(x[2][0])
-x[2].append(4)
-#print(x)
+#x = [1, 2, 3, 4, 5]
+#print(x[:2])
+#print(x[:])
+#x =range(5)
+#for i in x:
+#   print (i, i*i, i*i*i,)
 
-#for i in range(10):
-#    print (i, i*i, i*i*i)
+##27_
+#def cumulative_sum(a):
+#    d = []
+    
+#    for i in a:
+#        d.append(sum(a[:i]))
+#    return d
 
+
+#print(cumulative_sum(x))
+#print(cumulative_sum([4,3,2,1])) 
+#This reverse doesnt print out the way expected: [4, 7, 9, 10]
+
+##32_ Write a function lensort to sort a list of strings based on length
+languages = ["python", "perl", "java", "c", "haskell", "ruby"]
+def lensort(a):
+   return a.sort(key = lambda x: len(x))
+
+#print(languages)
+lensort(languages)
+print(languages)
+
+##35_ Write a function extsort to sort a list of files based on extension
+extensions = ['a.c','a.py', 'foo.txt', 'bar.txt', 'b.py', 'x.c']
+def extsort(a):
+    i = 0
+    while(i<len(a)):
+        a[i] =a[i].split('.')
+        i = i + 1
+    a.sort(key = lambda x: x[1])
+    #print(a)
+    i = 0
+    while(i<len(a)):
+        a[i] ='.'.join(a[i])
+        i = i + 1
+    #print(a)
+    return a
+          
+#print(extensions)
+extsort(extensions)
+print(extensions)
+
+##Example: Word Count
+
+def wordcount(filename):
+    return len(open(filename).read().split())
+
+def linecount(filename):
+    return len(open(filename).readlines())
+
+def printLines(filename):
+    i = 1
+    for line in (open(filename).readlines()):
+        print(line.strip('\n') + ' ['+ str(i) +']')
+        i = i + 1
+
+def reverseLines(filename):
+    lineCount = len(open(filename).readlines())
+    lines = open(filename).readlines()
+    while(lineCount > 0):
+        print(lines[lineCount-1].strip('\n') + ' ['+ str(lineCount) + ']')
+        lineCount = lineCount - 1
+    
+
+print(wordcount("sheShells.txt"))
+print(linecount("sheShells.txt"))
+printLines("sheShells.txt")
+print()
+reverseLines("sheShells.txt")
