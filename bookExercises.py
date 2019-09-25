eb5ca1cb9b4e8a46a93a46c31a0baf0975ac7aa9
bco8upv <bryan_oreilly@homedepot.com>
2019-09-24 20:05:08 -0400

    Made it through the  Word Count example

--- bookExercises.py
+++ bookExercises.py
@@ -61,7 +61,10 @@ def extsort(a):
 extsort(extensions)
 print(extensions)
 
-##Example: Word Count
+##Example: Word Count ##
+
+def charcount(filename):
+    return len(open(filename).read())
 
 def wordcount(filename):
     return len(open(filename).read().split())
@@ -75,6 +78,7 @@ def printLines(filename):
         print(line.strip('\n') + ' ['+ str(i) +']')
         i = i + 1
 
+##36_
 def reverseLines(filename):
     lineCount = len(open(filename).readlines())
     lines = open(filename).readlines()
@@ -82,9 +86,74 @@ def reverseLines(filename):
         print(lines[lineCount-1].strip('\n') + ' ['+ str(lineCount) + ']')
         lineCount = lineCount - 1
     
-
+print(charcount("sheShells.txt"))
 print(wordcount("sheShells.txt"))
 print(linecount("sheShells.txt"))
 printLines("sheShells.txt")
 print()
-reverseLines("sheShells.txt")
+#reverseLines("sheShells.txt")
+
+##37_
+def reverseTheLine(filename):
+    reverseLine = ""
+    full = []
+    file = open(filename)
+    for line in file:
+        for letter in line:
+            if(letter != '\n'):
+                reverseLine = letter + reverseLine
+            elif(letter == '\n'):
+                print(reverseLine)
+                full.append(reverseLine)
+                reverseLine = ""
+    #probably a poor way to do this.    
+    #This skips the last line if there is no newline at the end of it
+
+reverseTheLine("sheShells.txt")
+
+##38_ heads&tails unix commands to print first and last 10 lines
+#def headTailLines(filename):
+#    print("Maybe another time. not trying unix commands on windows today")
+
+##39_ grep the string. Print the lines
+#def grepTheString():
+#    print("same. not trying unix commands on windows today")
+print()
+##40_ takes file name and width, then wraps lines longer than width
+def wrapByWidth(filename, width):
+    f = open(filename)
+    lines = f.readlines()
+    for line in lines:
+        i = 0
+        for letter in line:
+            if(i > width-1):
+                print()
+                i = 0
+            print(letter, end="")
+            i = i + 1
+            #print(i)
+            
+
+wrapByWidth("sheShells.txt", 15)
+print()
+
+##41_ better word wrap that doesnt break words apart
+def wrapByWidth2(filename, width):
+    f = open(filename)
+    lines = f.readlines()
+    for line in lines:
+        i = 0
+        for letter in line:
+            if((i > width-1) & (letter == " ")):
+                print(letter)
+                i = 0
+            else:
+                print(letter, end="")
+                i = i + 1
+            
+    
+wrapByWidth2("sheShells.txt", 15)
+
+##42_ center align all lines in a file
+def center_align():
+    print()
